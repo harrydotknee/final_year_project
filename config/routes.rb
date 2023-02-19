ef4b6a16a877ac42e398c83a0a6f789b12805c63
Harry Knee <harry.knee@outlook.com>
2023-02-19 17:46:56 +0000

    add get exercises route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   get 'workouts', to: 'workouts#index'
   post 'workouts', to: 'workouts#create'
+  get 'exercises', to: 'exercises#index'
 end
