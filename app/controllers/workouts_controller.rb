fc7d96fb22f763ddcd8d75c24feba53174f2af28
Harry Knee <harry.knee@outlook.com>
2023-03-02 14:14:04 +0000

    add edit to workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,20 +1,26 @@
 class WorkoutsController < ApplicationController
     before_action :authenticate_user!
     def index
-        @workouts = Workout.all.filter { |workout| workout if workout.users.include?(current_user) }
-        render :json => @workouts, include: [:exercises => { :only => [:id, :name] }]
+      workouts = Workout.all.filter { |workout| workout if workout.users.include?(current_user) }
+      render :json => workouts, include: [:exercises => { :only => [:id, :name] }]
     end
 
     def create
-        workout_params = {
-          name: params[:name],
-        }
-        exercises = params[:exercises]
-        @workout = Workout.create(workout_params)
-        @workout.users << current_user
-        exercises.each do |exercise|
-          @workout.add_exercise(Exercise.find_by(name: exercise[:name]))
-        end
-        render :json => @workout, include: [:exercises => { :only => [:id, :name] }]
+      exercises = params[:workout][:exercises]
+      workout = Workout.create(name: params[:workout][:name])
+      workout.users << current_user
+      exercises.each do |exercise|
+        workout.add_exercise(Exercise.find_by(name: exercise))
+      end
+      render :json => @workout, include: [:exercises => { :only => [:id, :name] }]
+    end
+
+    def edit
+      workout = Workout.find(params[:id])
+      exercises = params[:exercises]
+      debugger
+      new_exercises = exercises.map { |exercise| Exercise.find_by(name: exercise["name"]) }
+      workout.update(name: params[:name], exercises: new_exercises)
+      render :json => workout, include: [:exercises => { :only => [:id, :name] }]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   get 'workouts', to: 'workouts#index'
   post 'workouts', to: 'workouts#create'
+  post 'workouts/:id', to: 'workouts#edit'
   get 'exercises', to: 'exercises#index'
 end
